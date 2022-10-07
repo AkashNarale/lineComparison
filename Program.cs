6f7aca93db071431c10617a8fd8fbcbbb1468d83
AkashNarale <akashnarale43@gmail.com>
2022-10-07 15:20:55 +0530

    lineuc1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lineComparison
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+            lineUC3.UC3();
+            //lineUC2.UC2();
+            //lineUC1.UC1(20, 1, 16, 40);
+            Console.ReadKey();
+        }
+    }
+}
